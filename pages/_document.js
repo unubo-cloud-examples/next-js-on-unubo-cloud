ad88964e364564906ead1952053c5ccb915e3e4e
Marcel Thomas <marcelthomas5@gmail.com>
2019-01-29 08:56:22 +0000

    Added example files

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no,  shrink-to-fit=no"
+          />
+          <link rel="shortcut icon" href="//static.unubo.cloud/favicon.ico" />
+          <link rel="stylesheet" href="//static.unubo.cloud/style.css" />
+      
+          <title>Node.js - Unubo Cloud</title>
+        </Head>
+
+        <body class="build-next-js">
+          <Main />
+          <NextScript />
+        </body>
+      </html>      
+    )
+  }
+}
